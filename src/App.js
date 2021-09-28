b971b6e5bd416ed107bb15a5a4b254f0217e194b
Kewei Zhang <jeffzhkw@gmail.com>
2021-09-28 08:52:47 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route } from "react-router-dom";
 import './App.css';
+import Home from './containers/Home'
 
 function App() {
   return (
     <div>
       <p>Hello</p>
+      <Router>
+        <Switch>
+          <Route path = '/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
